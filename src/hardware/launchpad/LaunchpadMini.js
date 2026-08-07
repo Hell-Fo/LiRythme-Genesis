8f7f90dcf606eeccfe9cfcbc096422474cac782c
Daniel Coupal <dcoupal@hotmail.com>
2026-08-07 15:05:20 -0400

    Genesis 1:7 - And the land was divided

--- src/hardware/launchpad/LaunchpadMini.js
+++ src/hardware/launchpad/LaunchpadMini.js
@@ -16,10 +16,13 @@ Elias (OpenAI)
 ===============================================================================
 */
 
+import { LaunchpadMapping } from "./LaunchpadMapping.js";
+
 export class LaunchpadMini {
     constructor(midiManager) {
         this.midiManager = midiManager;
         this.deviceName = "Launchpad Mini";
+        this.mapping = LaunchpadMapping;
     }
 
     setLed(note, value) {
@@ -34,6 +37,54 @@ export class LaunchpadMini {
             this.deviceName,
             [0xB0, controller, value]
         );
+    }
+
+    getControlType(status, number) {
+        if (status === 0xB0) {
+            if (this.mapping.TOP_BUTTONS.includes(number)) {
+                return "TOP_BUTTON";
+            }
+        }
+
+        if (status === 0x90) {
+            if (this.mapping.MOTIF.includes(number)) {
+                return "MOTIF";
+            }
+
+            if (this.mapping.INSTRUMENTS.includes(number)) {
+                return "INSTRUMENT";
+            }
+
+            if (this.mapping.BOOKS.includes(number)) {
+                return "BOOK";
+            }
+
+            if (this.mapping.VALUE.includes(number)) {
+                return "VALUE";
+            }
+        }
+
+        return "UNKNOWN";
+    }
+
+    getControlPosition(controlType, number) {
+        let controls;
+
+        if (controlType === "MOTIF") {
+            controls = this.mapping.MOTIF;
+        } else if (controlType === "INSTRUMENT") {
+            controls = this.mapping.INSTRUMENTS;
+        } else if (controlType === "BOOK") {
+            controls = this.mapping.BOOKS;
+        } else if (controlType === "VALUE") {
+            controls = this.mapping.VALUE;
+        } else if (controlType === "TOP_BUTTON") {
+            controls = this.mapping.TOP_BUTTONS;
+        } else {
+            return null;
+        }
+
+        return controls.indexOf(number);
     }
 
     handleMidiMessage(data) {
